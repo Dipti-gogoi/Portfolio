c322d07d350570ea88f6a0c27ebce2a9a34be368
DIPTI GOGOI <65372599+Dipti-gogoi@users.noreply.github.com>
2020-07-08 18:35:57 -0600

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -2,7 +2,6 @@
 
  if (isset($_POST['submit'])) {
  	$name = $_POST['name'];
- 	$subject = $_POST['subject'];
  	$mailFrom = $_POST['mail'];
  	$message = $_POST['message'];
 
@@ -10,6 +9,6 @@
  	$headers = "From: ".$mailFrom;
  	$txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
- 	mail($mailTo, $subject, $txt, $headers);
+ 	mail($mailTo, $txt, $headers);
  	header("Location; index.php?mailsend");
  }
