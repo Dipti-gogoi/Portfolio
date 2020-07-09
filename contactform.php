5e7bc195f93ef6442eac9fa7472fbbc72363ddb8
DIPTI GOGOI <65372599+Dipti-gogoi@users.noreply.github.com>
2020-07-08 18:59:59 -0600

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,14 +1,15 @@
 <?php 
 
- if (isset($_POST['submit'])) {
- 	$name = $_POST['name'];
- 	$mailFrom = $_POST['mail'];
- 	$message = $_POST['message'];
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
 
- 	$mailTo = "dipti17gogoi@gmail.com";
- 	$headers = "From: ".$mailFrom;
- 	$txt = "You have received an e-mail from ".$name.".\n\n".$message;
+    $mailTo = "diptigogoi@hotmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
- 	mail($mailTo, $txt, $headers);
- 	header("Location; index.php?mailsend");
- }
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
